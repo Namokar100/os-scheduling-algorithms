ace3954e7c557667c0d0eb51ad612f508ca54054
Namokar100 <namokarsvg@gmail.com>
2025-07-31 20:34:42 +0530

    add route to visualize page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Visualize from "./components/Visualize";
+import { Routes, Route } from "react-router";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/visualize" element={<Visualize />} />
+      </Routes>
     </div>
   );
 };
